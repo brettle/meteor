359be4b5b6f2dcb1c2c0cc689cfd2989ccebf8ce
Sashko Stubailo <sashko@stubailo.com>
2015-07-28 11:09:38 -0700

    Further class-ify HtmlScanner

--- packages/html-scanner/html-scanner.js
+++ packages/html-scanner/html-scanner.js
@@ -8,32 +8,11 @@ HtmlScanner = class HtmlScanner {
 
   // Note: source_name is only used for errors (so it's not part of the cache
   // key in compile-templates.js).
-  scan(contents, source_name) {
-    var rest = contents;
-    var index = 0;
-
-    var advance = function(amount) {
-      rest = rest.substring(amount);
-      index += amount;
-    };
-
-    var throwSpecialError = function (msg, errorClass, overrideIndex) {
-      var ret = new errorClass;
-      ret.message = msg;
-      ret.file = source_name;
-      var theIndex = (typeof overrideIndex === 'number' ? overrideIndex : index);
-      ret.line = contents.substring(0, theIndex).split('\n').length;
-      throw ret;
-    };
-    var throwParseError = function (msg, overrideIndex) {
-      throwSpecialError(
-        msg || "bad formatting in template file",
-        HtmlScanner.ParseError,
-        overrideIndex);
-    };
-    var throwBodyAttrsError = function (msg) {
-      throwSpecialError(msg, HtmlScanner.BodyAttrsError);
-    };
+  scan(contents, sourceName) {
+    this.rest = contents;
+    this.index = 0;
+    this.sourceName = sourceName;
+    this.contents = contents;
 
     var results = {};
     results.head = '';
@@ -44,13 +23,13 @@ HtmlScanner = class HtmlScanner {
     tagNames = ["body", "head", "template"].join("|");
     var rOpenTag = new RegExp(`^((<(${tagNames})\\b)|(<!--)|(<!DOCTYPE|{{!)|$)`, "i");
 
-    while (rest) {
+    while (this.rest) {
       // skip whitespace first (for better line numbers)
-      advance(rest.match(/^\s*/)[0].length);
+      this.advance(this.rest.match(/^\s*/)[0].length);
 
-      var match = rOpenTag.exec(rest);
+      var match = rOpenTag.exec(this.rest);
       if (! match)
-        throwParseError("Expected <template>, <head>, or <body> tag" +
+        this.throwParseError("Expected <template>, <head>, or <body> tag" +
                         " in template file");
 
       var matchToken = match[1];
@@ -58,29 +37,29 @@ HtmlScanner = class HtmlScanner {
       var matchTokenComment = match[4];
       var matchTokenUnsupported = match[5];
 
-      var tagStartIndex = index;
-      advance(match.index + match[0].length);
+      var tagStartIndex = this.index;
+      this.advance(match.index + match[0].length);
 
       if (! matchToken)
         break; // matched $ (end of file)
       if (matchTokenComment === '<!--') {
         // top-level HTML comment
-        var commentEnd = /--\s*>/.exec(rest);
+        var commentEnd = /--\s*>/.exec(this.rest);
         if (! commentEnd)
-          throwParseError("unclosed HTML comment in template file");
-        advance(commentEnd.index + commentEnd[0].length);
+          this.throwParseError("unclosed HTML comment in template file");
+        this.advance(commentEnd.index + commentEnd[0].length);
         continue;
       }
       if (matchTokenUnsupported) {
         switch (matchTokenUnsupported.toLowerCase()) {
         case '<!doctype':
-          throwParseError(
+          this.throwParseError(
             "Can't set DOCTYPE here.  (Meteor sets <!DOCTYPE html> for you)");
         case '{{!':
-          throwParseError(
+          this.throwParseError(
             "Can't use '{{! }}' outside a template.  Use '<!-- -->'.");
         }
-        throwParseError();
+        this.throwParseError();
       }
 
       // otherwise, a <tag>
@@ -89,11 +68,11 @@ HtmlScanner = class HtmlScanner {
       var rTagPart = /^\s*((([a-zA-Z0-9:_-]+)\s*=\s*(["'])(.*?)\4)|(>))/;
       var attr;
       // read attributes
-      while ((attr = rTagPart.exec(rest))) {
+      while ((attr = rTagPart.exec(this.rest))) {
         var attrToken = attr[1];
         var attrKey = attr[3];
         var attrValue = attr[5];
-        advance(attr.index + attr[0].length);
+        this.advance(attr.index + attr[0].length);
         if (attrToken === '>')
           break;
         // XXX we don't HTML unescape the attribute value
@@ -104,36 +83,61 @@ HtmlScanner = class HtmlScanner {
         tagAttribs[attrKey] = attrValue;
       }
       if (! attr) // didn't end on '>'
-        throwParseError("Parse error in tag");
+        this.throwParseError("Parse error in tag");
       // find </tag>
-      var end = (new RegExp('</'+tagName+'\\s*>', 'i')).exec(rest);
+      var end = (new RegExp('</'+tagName+'\\s*>', 'i')).exec(this.rest);
       if (! end)
-        throwParseError("unclosed <"+tagName+">");
-      var tagContents = rest.slice(0, end.index);
-      var contentsStartIndex = index;
+        this.throwParseError("unclosed <"+tagName+">");
+      var tagContents = this.rest.slice(0, end.index);
+      var contentsStartIndex = this.index;
 
       if (tagName === 'body') {
-        this._addBodyAttrs(results, tagAttribs, throwBodyAttrsError);
+        this.addBodyAttrs(results, tagAttribs);
       }
 
       // act on the tag
       handleTag(results, tagName, tagAttribs, tagContents,
-                              throwParseError, contentsStartIndex,
+                              this.throwParseError.bind(this), contentsStartIndex,
                               tagStartIndex);
 
       // advance afterwards, so that line numbers in errors are correct
-      advance(end.index + end[0].length);
+      this.advance(end.index + end[0].length);
     }
 
     return results;
   }
 
-  _addBodyAttrs(results, attrs, throwBodyAttrsError) {
-    Object.keys(attrs).forEach(function (attr) {
+  advance(amount) {
+    this.rest = this.rest.substring(amount);
+    this.index += amount;
+  }
+
+  throwSpecialError(msg, errorClass, overrideIndex) {
+    var ret = new errorClass;
+    ret.message = msg;
+    ret.file = this.sourceName;
+    var theIndex = (typeof overrideIndex === 'number' ? overrideIndex : this.index);
+    ret.line = this.contents.substring(0, theIndex).split('\n').length;
+    throw ret;
+  }
+
+  throwParseError(msg, overrideIndex) {
+    this.throwSpecialError(
+      msg || "bad formatting in template file",
+      HtmlScanner.ParseError,
+      overrideIndex);
+  }
+
+  throwBodyAttrsError(msg) {
+    this.throwSpecialError(msg, HtmlScanner.BodyAttrsError);
+  }
+
+  addBodyAttrs(results, attrs) {
+    Object.keys(attrs).forEach((attr) => {
       var val = attrs[attr];
 
       if (results.bodyAttrs.hasOwnProperty(attr) && results.bodyAttrs[attr] !== val) {
-        throwBodyAttrsError(
+        this.throwBodyAttrsError(
           "<body> declarations have conflicting values for the '" + attr + "' attribute.");
       }
 
