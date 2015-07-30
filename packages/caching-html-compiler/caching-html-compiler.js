5c76f2d3129250acabb349e5d0a8bac0b70b3901
Sashko Stubailo <sashko@stubailo.com>
2015-07-29 18:38:43 -0700

    Add README for caching-html-compiler

--- packages/caching-html-compiler/caching-html-compiler.js
+++ packages/caching-html-compiler/caching-html-compiler.js
@@ -3,6 +3,16 @@ const path = Plugin.path;
 // The CompileResult type for this CachingCompiler is the return value of
 // htmlScanner.scan: a {js, head, body, bodyAttrs} object.
 CachingHtmlCompiler = class CachingHtmlCompiler extends CachingCompiler {
+  /**
+   * Constructor for CachingHtmlCompiler
+   * @param  {String} name The name of the compiler, printed in errors -
+   * should probably always be the same as the name of the build
+   * plugin/package
+   * @param  {Function} tagScannerFunc Transforms a template file (commonly
+   * .html) into an array of Tags
+   * @param  {Function} tagHandlerFunc Transforms an array of tags into a
+   * results object with js, body, head, and bodyAttrs properties
+   */
   constructor(name, tagScannerFunc, tagHandlerFunc) {
     super({
       compilerName: name,
@@ -15,6 +25,7 @@ CachingHtmlCompiler = class CachingHtmlCompiler extends CachingCompiler {
     this.tagHandlerFunc = tagHandlerFunc;
   }
 
+  // Implements method from CachingCompilerBase
   compileResultSize(compileResult) {
     function lengthOrZero(field) {
       return field ? field.length : 0;
@@ -23,17 +34,20 @@ CachingHtmlCompiler = class CachingHtmlCompiler extends CachingCompiler {
       lengthOrZero(compileResult.js);
   }
 
+  // Overrides method from CachingCompiler
   processFilesForTarget(inputFiles) {
     this._bodyAttrInfo = {};
     super.processFilesForTarget(inputFiles);
   }
 
+  // Implements method from CachingCompilerBase
   getCacheKey(inputFile) {
     // Note: the path is only used for errors, so it doesn't have to be part
     // of the cache key.
     return inputFile.getSourceHash();
   }
 
+  // Implements method from CachingCompiler
   compileOneFile(inputFile) {
     const contents = inputFile.getContentsAsString();
     const inputPath = inputFile.getPathInPackage();
@@ -58,6 +72,7 @@ CachingHtmlCompiler = class CachingHtmlCompiler extends CachingCompiler {
     }
   }
 
+  // Implements method from CachingCompilerBase
   addCompileResult(inputFile, compileResult) {
     if (compileResult.head) {
       inputFile.addHtml({ section: "head", data: compileResult.head });
